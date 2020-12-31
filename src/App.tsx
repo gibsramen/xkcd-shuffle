c119d6ebcf0684fc3795cffb4b68061bc6ed1add
Gibraan Rahman <grahman@eng.ucsd.edu>
2020-12-31 12:25:37 -0600

    default behavior for development

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import axios from 'axios';
 import './App.css';
 
 const apiUrl = "https://ohls623gud.execute-api.us-west-1.amazonaws.com/default/return-random-xkcd-panels";
+const defaultImgLinks: string[] = [];
+for (let i = 0; i < 5; i++) {
+  defaultImgLinks.push("https://i.kym-cdn.com/entries/icons/mobile/000/024/523/sad.jpg");
+}
 
 function getRandomPanels(numPanels: number) {
   return axios.post(apiUrl, {numPanels: numPanels});
@@ -13,13 +17,15 @@ function getRandomPanels(numPanels: number) {
 const App = () => {
   const [numPanels, setNumPanels] = useState(3);
   const [selectedValue, setSelectedValue] = useState(3);
-  const [imgLinks, setImgLinks] = useState([]);
+  const [imgLinks, setImgLinks] = useState(defaultImgLinks);
   const [isComicPresent, setIsComicPresent] = useState(false); //don't display anything to start
 
   useEffect( () => {
-    getRandomPanels(numPanels).then( response => {
-      setImgLinks(response.data.img_links);
-    });
+    if (process.env.NODE_ENV === "production") {
+      getRandomPanels(numPanels).then( response => {
+        setImgLinks(response.data.img_links);
+      });
+    }
   }, [numPanels])
 
   const selectNumPanels = (event: ChangeEvent<HTMLSelectElement>) => {
