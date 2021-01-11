0d1b7989209de5ebeb0ae7b93b4a013f68d7d33a
Gibraan Rahman <grahman@eng.ucsd.edu>
2021-01-10 20:57:39 -0600

    preliminary support for loading indicator

--- src/components/Comic/Comic.tsx
+++ src/components/Comic/Comic.tsx
@@ -1,23 +1,44 @@
+import { useRef, useState } from 'react';
 import './Comic.css';
 
 interface ComicProps {
   numPanels: number,
   imgLinks: Array<string>,
-  comicsUsed: Array<string>
+  comicsUsed: Array<string>,
 };
 
 const Comic = (props: ComicProps) => {
   const altText = props.comicsUsed.map((comic) => "comic-" + comic)
+
+  //https://stackoverflow.com/a/56903585
+  const [loading, setLoading] = useState(true);
+  const counter = useRef(0);
+  const imgLoaded = () => {
+    counter.current += 1;
+    if (counter.current >= props.numPanels) {
+      setLoading(false);
+      counter.current = 0;
+    } else {
+      setLoading(true);
+    }
+  };
+
   const comicPanels = [];
   for (let i=0; i < props.numPanels; i++) {
     comicPanels.push(
-      <img src={props.imgLinks[i]} key={i} alt={altText[i]}/>
+      <img
+        src={props.imgLinks[i]}
+        key={i}
+        alt={altText[i]}
+        onLoad={imgLoaded}
+      />
     )
   }
 
   return (
     <div id="comic-container">
-      <div id="comic">{comicPanels}</div>
+      <div id="loading" style={{display: loading ? "block" : "none"}}>Loading...</div>
+      <div id="comic" style={{display: loading ? "none" : "inline-block"}}>{comicPanels}</div>
       <ComicInfo comicsList={props.comicsUsed} numPanels={props.numPanels} />
     </div>
   )
